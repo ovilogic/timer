1ebbb833f05e99c26520789ce3acca3a5672c0f6
ovilogic <ovidiu.opreah@yahoo.com>
2022-04-06 18:23:49 +0100

    limited functionality

--- src/App.js
+++ src/App.js
@@ -1,31 +1,115 @@
+
+import { useState } from 'react';
 import './style/app.css'
 
 
 function App() {
+
+  const [total, setTotal] = useState('')
+  const [main, setMain] = useState('')
+  const [plus, setPlus] = useState(false)
+  const [minus, setMinus] = useState(false)
+  const [display, setDisplay] = useState('')
+
+  let numbers = []
+  for (let i = 0; i < 10; i++) {
+    numbers.push(i)
+  }
+
+  console.log(typeof main, typeof total)
+
+
+
+  const handleNumber = (number) => {
+
+    if (typeof total == 'string') {
+      setTotal(total.concat(number))
+    }
+    if (typeof main == 'string') {
+      setMain(number)
+    }
+    else {setMain(String(main).concat(number))}
+    
+    
+    setDisplay(display.concat(number))
+    if (plus) {
+      console.log(typeof total, ' is type of total')
+      setTotal(total + number)
+      
+      setPlus(false)
+      
+      
+    }
+    else if (minus) {
+      setTotal(total - number)
+      setMinus(false)
+    }
+  }
+  
+  let numbPad = () => 
+    numbers.map(number => 
+      <div className='grid-item number'
+      id={'no'.concat(number)}
+      onClick={() => handleNumber(number)}>
+        
+        <p>{number}</p>
+      </div>
+      )
+  
+
+  console.log('main =', main, 'total =', total)
+ 
+  
+
+
   return (
     <div className='page'>
 
       <div className="App">
-        <div class="display">0</div>
-        <div class="grid-item AC">
+        {numbPad()}
+        <div className="display" id='display'>
+          <div className='display1'>
+            <p>{display}</p>
+          </div>
+          <div className='display2'>
+            <p>{main}</p>
+          </div>
+        </div>
+        <div className="grid-item AC"
+          id='clear'
+          onClick={() => {
+            setTotal('');
+            setMain('');
+            setPlus(false);
+            setMinus(false)
+            setDisplay('')}}>
           <p>AC</p>
         </div>
-        <div class="grid-item operator"><p>/</p></div>
-        <div class="grid-item operator"><p> X</p></div>
-        <div class="grid-item number"><p>7 </p></div>
-        <div class="grid-item number"><p> 8</p></div>
-        <div class="grid-item number"><p> 9</p></div>
-        <div class="grid-item operator"><p> -</p></div>
-        <div class="grid-item number"><p>4 </p></div>
-        <div class="grid-item number"><p> 5</p></div>
-        <div class="grid-item number"><p> 6</p></div>
-        <div class="grid-item operator"><p>+</p></div>
-        <div class="grid-item number"><p>1</p></div>
-        <div class="grid-item number"><p>2</p></div>
-        <div class="grid-item number"><p>3</p></div>
-        <div class="grid-item equal"><p>=</p></div>
-        <div class="grid-item zero"><p>0</p></div>
-        <div class="grid-item number"><p>.</p></div>
+        <div className="grid-item operator"><p>/</p></div>
+        <div className="grid-item operator"><p> X</p></div>
+       
+        <div className="grid-item operator"
+        onClick={() => {
+          setMinus(true);
+          setDisplay(display.concat('-'));
+          setMain('-');
+          }
+          }><p> -</p></div>
+        
+        <div className="grid-item operator"
+          onClick={() => {
+            setTotal(Number(total))
+            setPlus(true);
+            setDisplay(display.concat('+'))
+            setMain('+');
+            }
+          }><p>+</p></div>
+       
+        
+        <div className="grid-item equal"
+          onClick={() => setMain(total)}><p>=</p></div>
+        
+        <div className="grid-item number"><p>.</p></div>
           
       </div>
       
