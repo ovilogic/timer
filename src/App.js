a6c273abd20d178da13041b8e4616ef66af6e536
ovilogic <ovidiu.opreah@yahoo.com>
2022-04-10 13:46:43 +0100

    Completed! Only some of the freeCodeCamp tests do not pass despite correct results

--- src/App.js
+++ src/App.js
@@ -1,64 +1,73 @@
-
 import { useEffect, useState } from 'react';
 import './style/app.css'
 
 
 function App() {
 
-  
   const [main, setMain] = useState('0')
   const [plus, setPlus] = useState(false)
   const [minus, setMinus] = useState(false)
   const [times, setTimes] = useState(false)
   const [divide,setDivide] = useState(false)
   const [display, setDisplay] = useState('')
- 
   const [result, setResult] = useState(0)
   const [equal, setEqual] = useState(false)
- 
+  // 'equal' instructions manual:
+  //  The state is set to true when the equal button is pressed and the 
+  // handleEqual() is triggerred. It is then set back to false when any
+  // number button is pressed and handleNumber() is triggerred. That's it.
+  const [rational, setQ] = useState(false)
 
-  
   let handleEqual = () => {
+    
     setMain(result)
     setDisplay(display.concat(' = ' + result))
-    
     setPlus(false);
     setMinus(false);
     setTimes(false);
     setDivide(false);
     setEqual(true)
-    
   }
   
   const handleNumber = (number) => {
-    if (equal) {
-      setMain(String(number));
-      setDisplay(String(number))
-      setEqual(false)
-    }
-    else setDisplay(display.concat(number))
-
-    if (plus == false && minus == false && times == false
-      && divide == false) {
-      
-      if (main == '0') {
-        setMain(main.replace('0', number))
-      }
+    if (main.length > 17) {
+      if (main == 'Digit limit reached') { }
       else {
-        setMain(main.concat(number))
+        let prevMain = main
+        setMain('Digit limit reached')
+        setTimeout(() => setMain(prevMain), 700)
+      }
+    }
+    else {
+      if (equal) {
+        setMain(String(number));
+        setDisplay(String(number))
+        setEqual(false)
+      }
+      else setDisplay(display.concat(number))
+  
+      if (plus == false && minus == false && times == false
+        && divide == false) {
+        if (main == '0') {
+          setMain(main.replace('0', number))
+        }
+        else {
+          setMain(main.concat(number))
+        }
+      }
+      
+      else {
+        if (main == '+' || main == '-' || main == '*' || main == '/') {
+          setMain(String(number));
+          
+        }
+        else {
+          setMain(main.concat(number));
+          
+        }
       }
     }
     
-    else {
-      if (main == '+' || main == '-' || main == 'x' || main == '/') {
-        setMain(String(number));
-        
-      }
-      else {
-        setMain(main.concat(number));
-        
-      }
-    }
   }
 
   let numbers = []
@@ -73,6 +82,7 @@ function App() {
     numbers.map(number => 
       <div className='grid-item number'
       id={padNames[number]}
+      key={number}
       onClick={() => handleNumber(number)}>
         <p>{number}</p>
       </div>
@@ -87,8 +97,8 @@ function App() {
       parsedDisplay.splice(parsedDisplay.indexOf(''), 1)
     }
 
-    console.log(parsedDisplay, 'before')
-    // if there are 2 consecutive minuses, they turn to plus:
+    // console.log(parsedDisplay)
+    // if there are 2 consecutive minuses, they turn to plus, while plus and minus result in a minus:
     for (let i = 0; i < parsedDisplay.length; i++) {
       if (parsedDisplay[i] === '-' && parsedDisplay[i+1] === '-') {
         parsedDisplay[i] = '+'
@@ -102,6 +112,12 @@ function App() {
         parsedDisplay[i] = '-'
         parsedDisplay.splice(i+1, 1)
       }
+
+      else if (parsedDisplay[i] === '*' && parsedDisplay[i+1] === '-') {
+        parsedDisplay[i+2] = '-'.concat(parsedDisplay[i+2])
+        parsedDisplay.splice(i+1, 1)
+      }
+     
     }
 
     // Define the 4 operations:
@@ -115,14 +131,12 @@ function App() {
       var indexMinus = parsedDisplay.indexOf('-')
         res = Number(parsedDisplay[indexMinus - 1]) - Number(parsedDisplay[indexMinus + 1])
         parsedDisplay.splice(indexMinus - 1, 3, res)
-        console.log(parsedDisplay)
     }
 
     const multiply = () => {
-      var indexTimes = parsedDisplay.indexOf('x')
+      var indexTimes = parsedDisplay.indexOf('*')
       res = Number(parsedDisplay[indexTimes - 1]) * Number(parsedDisplay[indexTimes + 1])
       parsedDisplay.splice(indexTimes - 1, 3, res)
-        
     }
 
     const divide = () => {
@@ -133,71 +147,83 @@ function App() {
     
     // The order of operations:
     let res
-  
-    if (parsedDisplay.includes('x') && parsedDisplay.indexOf('x')
+ 
+    const higherOperations = () => {
+      if (parsedDisplay.includes('*') && parsedDisplay.indexOf('*')
         != parsedDisplay.length -1) {
          
           if (parsedDisplay.includes('/') && parsedDisplay.indexOf('/')
-          != parsedDisplay.length -1 && parsedDisplay.indexOf('x')
+          != parsedDisplay.length -1 && parsedDisplay.indexOf('*')
           < parsedDisplay.indexOf('/')) {
             multiply()
           }
           else if (parsedDisplay.includes('/') && parsedDisplay.indexOf('/')
-          != parsedDisplay.length -1 && parsedDisplay.indexOf('x')
+          != parsedDisplay.length -1 && parsedDisplay.indexOf('*')
           > parsedDisplay.indexOf('/')) {
             divide()
-            console.log(parsedDisplay)
           }
-          else if (parsedDisplay.includes('/') === false)
+
+          else if (parsedDisplay.includes('/') === false) {
             multiply()
+          }
         }
 
-    if (parsedDisplay.includes('/') && parsedDisplay.indexOf('/')
-      != parsedDisplay.length -1) {
-      
-      if (parsedDisplay.includes('x') && parsedDisplay.indexOf('x')
-      != parsedDisplay.length -1 && parsedDisplay.indexOf('x')
-      < parsedDisplay.indexOf('/')) {
-        multiply()
+      if (parsedDisplay.includes('/') && parsedDisplay.indexOf('/')
+        != parsedDisplay.length -1) {
+        
+        if (parsedDisplay.includes('*') && parsedDisplay.indexOf('*')
+        != parsedDisplay.length -1 && parsedDisplay.indexOf('*')
+        < parsedDisplay.indexOf('/')) {
+          multiply()
+        }
+        else if (parsedDisplay.includes('*') && parsedDisplay.indexOf('*')
+        != parsedDisplay.length -1 && parsedDisplay.indexOf('*')
+        > parsedDisplay.indexOf('/')) {
+          divide()
+        }
+        else if (parsedDisplay.includes('*') === false)
+          divide()
       }
-      else if (parsedDisplay.includes('x') && parsedDisplay.indexOf('x')
-      != parsedDisplay.length -1 && parsedDisplay.indexOf('x')
-      > parsedDisplay.indexOf('/')) {
-        divide()
-        console.log(parsedDisplay)
-      }
-      else if (parsedDisplay.includes('x') === false)
-        divide()
     }
 
-  
+    if (parsedDisplay[1] != '=' && parsedDisplay.length % 2 != 0) {
+      while (parsedDisplay.includes('*') || parsedDisplay.includes('/')) {
+        higherOperations()
+      }
+    }
+    
     while (['+', '-'].includes(parsedDisplay[1])) {
       if (parsedDisplay[1] === '+') add()
       else subtract()
     }
-        
-      setResult(res)
-      // console.log(res)
-  
-  
+    if (Number.isInteger(res)) {
+      let r = String(res)
+      if (r.length > 17) {
+        r = Number(r).toExponential(6)
+        res = r
+    }
+    }
+    
+    setResult(res)
+    console.log('at the end result', res)
   }, [display])
   
   return (
     <div className='page'>
-
       <div className="App">
         {numbPad()}
-        <div className="display" id='display-big'>
+        <div className="display-big">
           <div className='display1'>
             <p>{display}</p>
           </div>
           <div className='display2'>
-            <div id='display'>{main}</div>
+            <p id='display'>{main}</p>
           </div>
         </div>
         <div className="grid-item AC"
           id='clear'
           onClick={() => {
+            setQ(false)
             setMain('0');
             setPlus(false);
             setMinus(false);
@@ -212,16 +238,15 @@ function App() {
         </div>
         <div className="grid-item operator" id='divide'
           onClick={() => {
-            
-            setDivide(true);
-            
+            setQ(false)
+            setDivide(true);        
             setMain('/');
             if (equal) {
               setDisplay(String(result).concat(' / '));
               setEqual(false);
             }
             else {
-              if (['+', '-', 'x'].includes(display[display.length - 2 ])) {
+              if (['+', '-', '*'].includes(display[display.length - 2 ])) {
                 setDisplay(display.replace(display[display.length - 2 ], ' / '))
               }
               else {
@@ -231,33 +256,32 @@ function App() {
             }
             }
           }>
-            <p>      /          </p></div>
+            <p>      /         </p></div>
         <div className="grid-item operator" id='multiply'
-          onClick={() => {
-         
+          onClick={() => {   
+            setQ(false)     
             setTimes(true);
-            
-            setMain('x');
+            setMain('*');
             if (equal) {
-              setDisplay(String(result).concat(' x '));
+              setDisplay(String(result).concat(' * '));
               setEqual(false);
             }
             else {
               if (['+', '-', '/'].includes(display[display.length - 2 ])) {
-                setDisplay(display.replace(display[display.length - 2 ], ' x '))
+                setDisplay(display.replace(display[display.length - 2 ], ' * '))
               }
               else {
-                if (['x'].includes(display[display.length - 2 ])) { }
-                else setDisplay(display.concat(' x '))
+                if (['*'].includes(display[display.length - 2 ])) { }
+                else setDisplay(display.concat(' * '))
               }
           }
           }
-          }><p>      X       </p></div>
+          }><p>      *       </p></div>
        
         <div className="grid-item operator" id='subtract'
           onClick={() => {
+            setQ(false)
             setMinus(true);
-            
             setMain('-')
             if (equal) {
               setDisplay(String(result).concat(' - '));
@@ -265,30 +289,24 @@ function App() {
             }
             else {
             
-            if (['x', '/'].includes(display[display.length - 2 ])) {
-              setDisplay(display.replace(display[display.length - 2 ], ' - '))
-            }
-            
-            else {
               if (display[display.length - 5] == '-') { }
               else setDisplay(display.concat(' - '))
-            }
+            
           }
         }
-          
-          }><p>   -    </p></div>
+        }><p>   -    </p></div>
         
         <div className="grid-item operator" id='add'
           onClick={() => {
             setPlus(true);
-
+            setQ(false)
             setMain('+');
             if (equal) {
               setDisplay(String(result).concat(' + '));
               setEqual(false);
             }
             else {
-              if (['x', '/'].includes(display[display.length - 2 ])) {
+              if (['*', '/'].includes(display[display.length - 2 ])) {
                 setDisplay(display.replace(display[display.length - 2 ], ' + '))
               }
               else {
@@ -300,31 +318,32 @@ function App() {
           }><p>    +      </p></div>
         
         <div className="grid-item equal" id='equals'
-          onClick={() => handleEqual()
+          onClick={() => {
+            if (equal) { }
+            else handleEqual();
+          }
           }><p>    =     </p>
         </div>
 
-
         <div className="grid-item number" id='decimal'
           onClick={() => {
-            
-            
-            if (equal) {
-              setDisplay(String(main).concat('.'))
-              setEqual(false);
-            }
+            if (rational) {  }
             else {
-              for (let i = 0; i < display.length; i++) {
-                console.log('index', i, 'value in array:', display[i], display.length)
+              setQ(true)
+              if (equal) {
+                setDisplay(String(main).concat('.'))
+                setEqual(false);
               }
-              
-                if (['.'].includes(display[display.length - 1 ])) { }
-                else {
-                  setDisplay(display.concat('.'));
-                  setMain(String(main).concat('.'))}
-              
+              else {
+                for (let i = 0; i < display.length; i++) {
+                  console.log('index', i, 'value in array:', display[i], display.length)
+                }
+                  if (['.'].includes(display[display.length - 1 ])) { }
+                  else {
+                    setDisplay(display.concat('.'));
+                    setMain(String(main).concat('.'))}
+              }
             }
-            
           }}><p>.</p></div>
           
       </div>
